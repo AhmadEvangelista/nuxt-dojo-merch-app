3886e34edaedb8d2a9826070baae0c6ff35116c4
AhmadEvangelista <ahmad.evangelista@gmail.com>
2022-12-05 22:37:46 +0800

    Error Handling

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -10,6 +10,14 @@ const uri = `https://fakestoreapi.com/products/${id}`;
 
 const { data: products } = await useFetch(uri, { key: id });
 
+if (!products.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Product not found',
+    fatal: true,
+  });
+}
+
 definePageMeta({
   layout: 'products',
 });
