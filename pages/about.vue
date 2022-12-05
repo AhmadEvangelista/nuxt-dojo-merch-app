a714d0dda61ff38a2edf5b25c5d903e8c8fb6b78
AhmadEvangelista <ahmad.evangelista@gmail.com>
2022-12-05 23:09:27 +0800

    Server Routes

--- pages/about.vue
+++ pages/about.vue
@@ -9,11 +9,18 @@
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quibusdam vel
       eaque autem aut inventore quos asperiores deserunt harum reprehenderit!
     </p>
+    <div>{{ data }}</div>
   </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+// const { data } = await useFetch('/api/ninja?name=mario', {
+//   method: 'post',
+//   body: {
+//     age: 30,
+//   },
+// });
+const { data } = await useFetch('/api/ninja');
 </script>
 
 <style scoped>
