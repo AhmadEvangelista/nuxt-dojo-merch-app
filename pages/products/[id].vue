7ef5f2bb8b3f8dd3fa84469d5ae91dcb75a372bb
AhmadEvangelista <ahmad.evangelista@gmail.com>
2022-12-05 20:00:53 +0800

    route parameters

---
+++ pages/products/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque perferendis
+      quibusdam ipsam commodi distinctio illo magni sit laborum ducimus
+      voluptatem.
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+</script>
+
+<style scoped></style>
