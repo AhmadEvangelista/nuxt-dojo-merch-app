617df74ea73742ec8c2abfa217bb2ecb454d7e3e
AhmadEvangelista <ahmad.evangelista@gmail.com>
2022-12-05 20:11:18 +0800

    nuxtlink

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <nuxt-link to="/">Nuxt Dojo</nuxt-link>
+        <ul>
+          <li>
+            <nuxt-link to="/">Home</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about">About</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/products">Products</nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
     <h2>Home</h2>
     <p>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et quibusdam vel
